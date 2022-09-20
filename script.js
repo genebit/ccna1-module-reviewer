fd23a628c6b6f11e4b31b0d34d8e935e85aa88a6
genebit <genebitara@gmail.com>
2022-09-20 11:19:41 +0800

    Refactored code

--- script.js
+++ script.js
@@ -1,55 +1,49 @@
 $.ajax({
-    dataType: "json",
-    url: "data.json",
-    success: function (res) {
-        let number = 1;
-        for (let i = 0; i < res["QUESTIONS"].length; i++) {
-            let title = res["QUESTIONS"][i].TITLE;
+	dataType: "json",
+	url: "data.json",
+	success: function (res) {
+		let number = 1
+		for (let i = 0; i < res["QUESTIONS"].length; i++) {
+			let title = res["QUESTIONS"][i].TITLE
 
-            let template = $("[questionnaire-card]")[0].content.cloneNode(true)
-                .children[0];
-            let header = template.querySelector("header");
-            header.querySelector("h6").textContent = `${number}. ${title}`;
-            let multioption = template.querySelector("multioption");
+			let template = $("[questionnaire-card]")[0].content.cloneNode(true).children[0]
+			let header = template.querySelector("header")
+			header.querySelector("h6").textContent = `${number}. ${title}`
+			let multioption = template.querySelector("multioption")
 
-            let answers = [];
-            let correct_answers = [];
+			let answers = []
+			let correct_answers = []
 
-            // getting and assigning the data for each element
-            res["QUESTIONS"][i].ANSWERS.forEach((element) => {
-                // if the word correct exist
-                if (element.includes("[/]")) {
-                    element = element.substring(
-                        element.search("[/]") + "[/]".length,
-                        element.length
-                    );
-                    correct_answers.push(element);
-                }
+			// getting and assigning the data for each element
+			res["QUESTIONS"][i].ANSWERS.forEach((element) => {
+				// if the word correct exist
+				if (element.includes("[/]")) {
+					element = element.substring(element.search("[/]") + "[/]".length, element.length)
+					correct_answers.push(element)
+				}
 
-                answers.push(element);
-            });
+				answers.push(element)
+			})
 
-            answers.forEach((element) => {
-                const node = document.createElement("option");
-                node.classList.add("card", "shadow-sm", "p-3");
-                node.setAttribute("value", element);
-                const textnode = document.createTextNode(element);
-                node.appendChild(textnode);
-                multioption.appendChild(node);
-            });
+			answers.forEach((element, i) => {
+				const node = document.createElement(`option_${i}`)
+				node.classList.add("card", "shadow-sm", "p-3")
+				node.setAttribute("value", element)
+				const textnode = document.createTextNode(element)
+				node.appendChild(textnode)
+				multioption.appendChild(node)
+			})
 
-            $("#Main").append(template);
-            number++;
-        }
-        $("option").click(function () {
-            let clickedValue = $(this).val();
-            if ($(this).hasClass("alert")) {
-                $(this).removeClass().addClass("card shadow-sm p-3");
-            } else {
-                $(this)
-                    .removeClass()
-                    .addClass("alert alert-primary shadow-sm p-3 m-0");
-            }
-        });
-    },
-});
+			$("content").append(template)
+			number++
+		}
+		$("multioption *").click(function () {
+			let clickedValue = $(this).val()
+			if ($(this).hasClass("alert")) {
+				$(this).removeClass().addClass("card shadow-sm p-3")
+			} else {
+				$(this).removeClass().addClass("alert alert-primary shadow-sm p-3 m-0")
+			}
+		})
+	},
+})
