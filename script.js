d3735ff35ceb6138624f05b4855f15e5fdf9b407
genebit <genebitara@gmail.com>
2022-09-20 12:39:59 +0800

    Implement checking

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 $.ajax({
+	url: "/resources/data.json",
 	dataType: "json",
-	url: "data.json",
 	success: function (res) {
 		let number = 1
+
 		for (let i = 0; i < res["QUESTIONS"].length; i++) {
 			let title = res["QUESTIONS"][i].TITLE
 
 			let template = $("[questionnaire-card]")[0].content.cloneNode(true).children[0]
 			let header = template.querySelector("header")
+			template.setAttribute("id", i)
+
 			header.querySelector("h6").textContent = `${number}. ${title}`
 			let multioption = template.querySelector("multioption")
 
@@ -25,25 +28,38 @@ $.ajax({
 				answers.push(element)
 			})
 
-			answers.forEach((element, i) => {
-				const node = document.createElement(`option_${i}`)
+			answers.forEach((answ, index) => {
+				const node = document.createElement(`option-${index}`)
 				node.classList.add("card", "shadow-sm", "p-3")
-				node.setAttribute("value", element)
-				const textnode = document.createTextNode(element)
+				const textnode = document.createTextNode(answ)
 				node.appendChild(textnode)
+
 				multioption.appendChild(node)
 			})
 
 			$("content").append(template)
 			number++
 		}
+
+		$("#CheckAnswersBtn").click(function () {
+			// loop through each card
+			// get the multioption selected answers
+			// if the selected answers is found within the array of correct answers
+		})
+
+		$("#ShowAnswersBtn").click(function () {
+			$("content card").each(function (index, element) {
+				$(`#${element.id} multioption *`)
+					.siblings()
+					.each(function (i, e) {
+						console.log(e)
+					})
+			})
+		})
+
 		$("multioption *").click(function () {
-			let clickedValue = $(this).val()
-			if ($(this).hasClass("alert")) {
-				$(this).removeClass().addClass("card shadow-sm p-3")
-			} else {
-				$(this).removeClass().addClass("alert alert-primary shadow-sm p-3 m-0")
-			}
+			let clickedValue = $(this).html()
+			$(this).toggleClass("alert alert-primary m-0")
 		})
 	},
 })
